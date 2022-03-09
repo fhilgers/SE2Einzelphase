455b73c827888022c349ccf0c12f279775322971
Felix Hilgers <felix_hilgers@posteo.de>
2022-03-09 03:12:20 +0100

    feat: implement simple view with TCP connection
    This implements a simple Android App View to send an
    immatriculation to the server 'se2-isys.aau.at' at
    port 53212 and display the response sent back.

--- app/src/main/java/com/github/se2einzelphase/MainActivity.java
+++ app/src/main/java/com/github/se2einzelphase/MainActivity.java
@@ -1,13 +1,46 @@
 package com.github.se2einzelphase;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText textInput;
+    TextView responseView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textInput = findViewById(R.id.textInput);
+        responseView = findViewById(R.id.responseView);
+    }
+
+    public void sendNumber(View view) {
+        new Thread(() -> {
+            try {
+                Socket client = new Socket("se2-isys.aau.at", 53212);
+                BufferedReader input = new BufferedReader(
+                        new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+                PrintWriter output = new PrintWriter(
+                        new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8),
+                        true);
+
+                output.println(textInput.getText().toString());
+                String response = input.readLine();
+
+                runOnUiThread(() -> responseView.setText(response));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 }
